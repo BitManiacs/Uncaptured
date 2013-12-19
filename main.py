4bbbd6822358edc606310ec504a2530877e2070e
bbaclig <bbaclig@ucsd.edu>
2013-12-18 20:07:41 -0800

    v0.2

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import pygame
+from pygame.locals import *
 
 ''' GLOBAL VARS '''
-
+SCREEN_PIXEL_WIDTH = 640
+SCREEN_PIXEL_HEIGHT = 480
+SCREEN_DIMENSION = (SCREEN_PIXEL_WIDTH, SCREEN_PIXEL_HEIGHT)
+clock = pygame.time.Clock()
 #game_engine = None #maybe dont need this
 
-def main():
-    # intlialize the game engine
-    #game_engine = makeGameEngine()
+class GameEngine():
+    def __init__(self):
+        # initilialize pygame
+        pygame.init()
+        # setup display screen
+        self.screen = pygame.display.set_mode(SCREEN_DIMENSION)
 
+    def draw(self):
+        # fill screen with darkness
+        self.screen.fill((0,0,0))
+        # update the display
+        pygame.display.flip()
+
+def main():
+    # make a GameEngine instance
+    game_engine = GameEngine()
     # continuously update game_engine
     while 1:
         for event in pygame.event.get():
