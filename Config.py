cf79960cff54fb50a3b37b4ce7336e2be3365c23
bbaclig <bbaclig@ucsd.edu>
2013-12-21 16:47:00 -0800

    incorporating Level instance

--- Config.py
+++ Config.py
@@ -7,3 +7,5 @@ Description: This file holds all the global variables that can be modified
 SCREEN_PIXEL_WIDTH = 640
 SCREEN_PIXEL_HEIGHT = 480
 SCREEN_DIMENSION = (SCREEN_PIXEL_WIDTH, SCREEN_PIXEL_HEIGHT)
+
+
---
+++ Level.py
@@ -0,0 +1,8 @@
+import pygame
+from pygame.locals import *
+
+class Level():
+    # @level: has to be a loaded json file.
+    def __init__(self,level):
+        # level has reference to data
+        self.data = level
